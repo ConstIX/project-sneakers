659c65ed8c08d47bd983173136a269744deb86ff
ConstIX <x.dilshd03@gmail.com>
2024-08-05 14:39:37 +0300

    create Header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from './components/Header.vue'
+import Home from './pages/Home.vue'
 </script>
 
 <template>
   <div class="mx-auto mt-20 h-screen w-4/5 rounded-t-2xl bg-white shadow-xl md3:w-full">
     <Header />
+    <Home />
   </div>
 </template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Card from '../components/Card.vue'
+</script>
+
+<template>
+  <div class="pb-6 pt-20">
+    <div class="container">
+      <div class="flex items-center justify-between">
+        <h1 class="text-3xl font-bold">Все кроссовки</h1>
+        <input class="" type="search" />
+      </div>
+
+      <div class="flex flex-wrap gap-9">
+        <Card />
+      </div>
+    </div>
+  </div>
+</template>
